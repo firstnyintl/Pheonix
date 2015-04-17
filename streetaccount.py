ae458ad25e05fb0f1d529f936709f58fa202e83b
Alex Kreindler <a.kreindler@firstny.com>
2015-04-17 11:46:41 -0400

    Added detailed documentation for scraper

--- streetaccount.py
+++ streetaccount.py
@@ -110,10 +110,14 @@ def getMessages():
         """
         Log into gmail using IMAP, return imaplib object
         """
+
+        # Login information
         USER = 'ratingsstock'
         PW = 'firstny2015'
         SERVER = 'imap.gmail.com'
         MAIN_FOLDER = 'inbox'
+
+        # Login and return mail object
         mail = imaplib.IMAP4_SSL(SERVER)
         mail.login(USER, PW)
         mail.list()
@@ -168,18 +172,48 @@ def getMessages():
 
     def getFirm(msg):
         """
-        Cross reference with firm database and return firm name
+        Cross reference with firm database and return firm name, if nothing found return emtpy string
         """
-        # Get firm name
+        # Get firm name from csv database via unique identifier
         csv_file = 'research_firm_list.csv'
         firm_list = DataFrame.from_csv(csv_file)
         firm_list['eval'] = firm_list['Unique Identifier'].isin(msg)
-        try:
-            firm = firm_list[firm_list['eval']].index.values[0]
-        except:
-            firm = ''
-        return firm
 
+        # If firm found in database, return firm name
+        try:
+            return firm_list[firm_list['eval']].index.values[0]
+
+        # If not, return blank string
+        except:
+            return ''
+
+    def getAnalyst(msg):
+        """
+        Extract analyst from message and return (assumed that message ends right after analyst or has another bullet)
+        If nothing found, return empty string
+        """
+        # If "Analyst" is found in message
+        if "Analyst" in msg:
+
+            # Get index of occurence
+            ix = msg.index('Analyst')
+
+            # Get index of beginning of analyst name (is there "is" in between?)
+            if msg[ix+1] != 'is': startix = ix+1
+            else: startix = ix+2
+
+            # Flatten rest of message
+            msg_substring = ' '.join(msg[startix:])
+
+            # If another bullet, set analyst name equal to rest of text before bullet
+            if '*' in msg_substring: return msg_substring[:msg_substring.index('*')]
+
+            # If no more bullets, assume end of message and set analyst to rest of message
+            else: return msg_substring
+
+        # If "Analyst" not found, return empty string
+        else:
+            analyst = ''
 
     def get_text_block(email_message_instance):
         """
@@ -197,54 +231,89 @@ def getMessages():
         """
         Process currecy identifiers for PT subsection of msg, returns currency string
         """
-        # If PT field doesnt contain currency identifier
+        # If currency is not directly attached to PT field (e.g. ['SEK','25.7'] vs. ['SEK25.7']
         if PT == msg[PTix]:
+
+            # Set currency equal to field immediately prior to PT field
             curr = msg[PTix-1]
-            # If identifier is $
+
+            # If prior field is '$'
             if curr == '$':
-                # If 'C' precedes $, currency is CAD
-                if msg[PTix-2] == 'C':
-                    return 'CAD'
-                # If 'C' precedes $, currency is CAD
-                if msg[PTix-2] == 'A':
-                    return 'AUD'
-                # If no 'C' precedes $, currency is USD
-                else:
-                    return 'USD'
+
+                # If 'C' precedes $, return 'CAD'
+                if msg[PTix-2] == 'C': return 'CAD'
+
+                # If 'A' precedes $, return 'AUD'
+                if msg[PTix-2] == 'A': return 'AUD'
+
+                # If nothing precedes $, return 'USD'
+                else: return 'USD'
+
+            # If prior field is not '$', return string (in these cases the field is the currency string)
             else: return curr
+
+        # If currency is directly attached to PT field (e.g. 'SEK25.7')
         else:
+
             # Seperate non digits from PT field
             curr = ''.join(i for i in msg[PTix] if not i.isdigit())
-            # Get rid of potential "."
+
+            # Get rid of potential "." left in string (e.g. '.SEK')
             curr = curr.replace('.', '')
-            # Check if 'p' in identifier, return GBP
-            if 'p' in curr:
-                return 'GBP'
-            # Check if Euro sign
-            if '\x80' in curr:
-                return 'EUR'
+
+            # If 'p' is identifier, return 'GBP' (e.g. '257p')
+            if 'p' in curr: return 'GBP'
+
+            # If '\x80' (Euro symbol) is identifier, return 'EUR' (e.g. '\x80270')
+            if '\x80' in curr: return 'EUR'
+
+            # If not 'GBP' or 'EUR', return extracted string (e.g. 'NOK', 'SEK', 'TWD')
             return curr
 
     def getPT(msg):
+        """
+        Get price target from message and return
+        """
+        # Search for "Target" and "target", if found get index within message
         if "Target" in msg: ix = msg.index('Target')
         elif 'target' in msg: ix = msg.index('target')
+
+        # If can't find, return empty strings for TP and curr
         else: return '', ''
+
+        # Loop through message after occurrence of "target"/"Target" until PT found
         for i, x in enumerate(msg[ix:]):
-            # Check if contains float
+
+            # If a float is found
             if len(re.findall('\d+.\d+', x)) > 0:
+
+                # Strip float (PT) from field
                 PT = re.findall('\d+.\d+', x)[0]
+
+                # Get currency
                 curr = getCurrency(PT, msg, ix+i)
+
+                # Convert string PT to float
                 PT = float(PT.replace(',', ''))
+
+                # Stop looking
                 break
-            # Check if contains int
+
+            # If an int is found
             elif len(re.findall('\d+', x)) > 0:
+
+                # Strip int (PT) from field
                 PT = re.findall('\d+', x)[0]
+
+                # Get currency
                 curr = getCurrency(PT, msg, ix+i)
+
+                # Convert string PT to float
                 PT = float(PT.replace(',', ''))
+
+                # Stop looking
                 break
-            # If doesn't contain float or int continue
-            else:
-                continue
+
         return PT, curr
 
     def getSingle(msg):
@@ -254,6 +323,9 @@ def getMessages():
         Returns a tuple: (boolean, list), where:
         --boolean: "True" if single event, otherwise "False"
         --list: [ticker, msgtype, rating, PT, curr, analyst]
+
+        Will return "True, None" if message type not recognized
+        Will return "False, None" if more than one event in message
         """
 
         # If msg contains any of the following, there are multiple events per message
@@ -262,52 +334,64 @@ def getMessages():
         # If rating starts with one of the following, it is a two-word rating (e.g. "sector perform")
         two_word_rating_list = ['sector', 'market', 'strong']
 
-        # Check whether is single upgrade / downgrade / initiation / resumption
+        # If no value in multiple_msg_tokens found in msg, assume single event
         if not any([e in msg for e in multiple_msg_tokens]):
 
-            # Get ticker
+            # Get ticker by looking at standard index in message
             SINGLE_MESSAGE_TICKER_INDEX = 5
             ticker = msg[SINGLE_MESSAGE_TICKER_INDEX]
 
-            # Get Analyst (assumed that message ends right after analyst or has another bullet)
-            if "Analyst" in msg:
-                ix = msg.index('Analyst')
-
-                # Find beginning of analyst name
-                if msg[ix+1] != 'is': startix = ix+1
-                else: startix = ix+2
-
-                # Get full msg substring until next bullet or end of msg, extract analyst
-                msg_substring = ' '.join(msg[startix:])
-                if '*' in msg_substring: analyst = msg_substring[:msg_substring.index('*')]
-                else: analyst = msg_substring
-
-            else:
-                analyst = ''
-
-            # Get message type and rating
+            # Look for analyst name
+            analyst = getAnalyst(msg)
 
             # Check if target price increase or decrease
-            # Assumed format 'target in/decreased to x' before first bullet or +5 spaces
+            # Assumed format 'target in/decreased to x' before first bullet or +5 spaces after ticker (headline)
             try:
                 stpindx = msg.index('*')
             except:
                 stpindx = 5
+
+            # If 'target' found in msg headline
             if 'target' in msg[SINGLE_MESSAGE_TICKER_INDEX:stpindx]:
+
+                # Get index of occurence
                 ix = msg.index('target')
+
+                # If followed by 'increased' or 'raises'
                 if msg[ix+1] == 'increased' or msg[ix-1] == 'raises':
+
+                    # Set type to 'PT increased'
                     msgtype = 'PT increased'
+
+                    # Get PT and currency
                     PT, curr = getPT(msg)
+
+                    # Set rating to empty string
                     rating = ''
+
+                # If followed by 'decreased' or 'lowers'
                 elif msg[ix+1] == 'decreased' or msg[ix-1] == 'lowers':
+
+                    # Set type to 'PT decreased'
                     msgtype = 'PT decreased'
+
+                    # Get PT and currency
                     PT, curr = getPT(msg)
+
+                    # Set rating to empty string
                     rating = ''
+
+                # If not followed by one of the above, msg type not understood
                 else:
                     return True, None
+
+                # Return True for single event, return values
                 return True, [ticker, msgtype, rating, PT, curr, analyst]
+
             # If not PT change, check for other msg types 
             else:
+
+                # Key/value dictionary for other message types in headline
                 key_dict = {'upgraded': 'upgrade',
                             'downgraded': 'downgrade',
                             'initiated': 'initiation',
@@ -318,32 +402,57 @@ def getMessages():
                             're-initiated': 'resumption',
                             'assumed': 'resumption',
                             }
+
+                # Message type recognized instantiated to False
                 msgtype_recognized = False
+
+                # Search dictionary for keywords
                 for key, value in key_dict.iteritems():
+
+                    # If keyword found
                     if key in msg:
+
+                        # Message recognized
                         msgtype_recognized = True
+
+                        # Set type according to dictionary
                         msgtype = value
+
+                        # Get index of keyword occurrence
                         ix = msg.index(key)
+
+                        # Assume keyword followed by 'to', if keyword in two_word_rating_list
                         if msg[ix+2] in two_word_rating_list:
+
+                            # Return two-word rating
                             rating = msg[ix+2] + ' ' + msg[ix+3]
+
+                        # If keyword not in two_word_rating list
                         else:
+
+                            # Return single-word rating
                             rating = msg[ix+2]
-                # If msg type not accounted for, exit
+
+                # If msg type not recognized, exit
                 if not msgtype_recognized: return True, None
 
                 # Get Price Target and currency
                 PT, curr = getPT(msg)
+
+                # Return True and values
                 return True, [ticker, msgtype, rating, PT, curr, analyst]
+
+        # More than one event in msg
         return False, None
 
     def processMsg(msg):
         """
         Process message, if successful, write to database, if not print why
         """
-        # Process date
+        # Look for date
         date = getDate(msg)
 
-        # Process firm name
+        # Look for firm name
         firm = getFirm(msg)
 
         # Check if email only contains one event (one ticker)
@@ -394,6 +503,6 @@ def getMessages():
         x, y = mail.uid('search', None, "(UNSEEN)")
         unreadMails = len(y[0].split()) > 0
 
-# If this is executed as script, i.e. from console "python streetaccount.py"
+# If file is executed as script
 if __name__ == '__main__':
     getMessages()
